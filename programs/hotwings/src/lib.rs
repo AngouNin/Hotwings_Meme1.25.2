4d325762371ae891f0bdb83d40a7601b71247e9f
AngouNin <bmars8218@gmail.com>
2025-01-16 01:11:59 +0900

    Import CSV data and Store all users info to Lock account

--- programs/hotwings/src/lib.rs
+++ programs/hotwings/src/lib.rs
@@ -1,9 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::*;
-use crate::states::*; 
+use anchor_spl::token::{Token, TokenAccount};
 
-pub mod states;
-pub mod consts;
 
 declare_id!("6vxBssG3FvWset4jv3STQGGnq3mTqkkD2BSbYC5s7j89");
 
@@ -18,6 +16,13 @@ pub mod hotwings {
         users: Vec<InvestorInfo>, // Batch of users
     ) -> Result<()> {
         let lock_pool = &mut ctx.accounts.lock_pool_account;
+
+         // Step 1: Initialize start_time if not already set
+        if lock_pool.start_time == 0 {
+            // Fetch current cluster time
+            let clock = Clock::get()?; // Gets the current clock (cluster time)
+            lock_pool.start_time = clock.unix_timestamp; // Set `start_time` using Solana clock
+        }
 
         for user in users.iter() {
             // Step 1: Transfer tokens to the shared lock pool token account
@@ -48,3 +53,38 @@ pub mod hotwings {
 
 }
 
+
+#[derive(Accounts)]
+pub struct InitializeLockAccounts<'info> {
+    #[account(mut)]
+    pub lock_pool_account: Account<'info, LockPoolState>, // Track all locking data for users
+    #[account(mut)]
+    pub lock_pool_token_account: Account<'info, TokenAccount>, // Shared lock vault (PDA-owned)
+    #[account(mut)]
+    pub source_wallet: Account<'info, TokenAccount>, // Admin's funding source wallet
+    #[account(mut)]
+    pub admin_wallet: Signer<'info>, // Wallet signing token transfers (Presale Manager)
+    pub token_program: Program<'info, Token>, // Standard SPL Token program
+    pub clock: Sysvar<'info, Clock>, // Add the SysvarClock system account to fetch cluster time
+}
+
+#[derive(Clone, AnchorSerialize, AnchorDeserialize, Debug)]
+pub struct InvestorInfo {
+    pub wallet_address: Pubkey,
+    pub token_amount: u64,
+}
+
+#[account]
+pub struct LockPoolState {
+    pub total_locked: u64,               // Total locked tokens in the pool
+    pub users: Vec<UserLockInfo>,       // List of all users and locked info
+    pub start_time: i64,  
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
+pub struct UserLockInfo {
+    pub user_wallet: Pubkey,            // Wallet address of the user
+    pub total_tokens: u64,              // Purchased tokens during presale
+    pub unlocked_tokens: u64,           // Unlocked tokens (via milestones)
+    pub locked_tokens: u64,             // Remaining locked tokens
+}
